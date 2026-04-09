e92a94f293e12414f9b60455d58f9bbef39a7c32
Ralph <ralph.hawkins1@nhs.net>
2026-04-09 15:01:45 +0100

    first commit

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Digital health platforms',
 
   // Port to run the prototype on locally
   port: 3000
